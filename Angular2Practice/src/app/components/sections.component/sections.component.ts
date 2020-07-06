f64c571d4ac6d17820e2b8deb3ea015b866f2dbf
ManiaR7 <ramrakhianimanish73@gmail.com>
2020-07-06 17:36:25 +0530

    Sections error fix

--- Angular2Practice/src/app/components/sections.component/sections.component.ts
+++ Angular2Practice/src/app/components/sections.component/sections.component.ts
@@ -3,24 +3,22 @@ import { NewsService } from '../../services/news.service'
 import { section } from '../../model/news'
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/delay';
-import { ROUTER_PROVIDERS } from '@angular/router/src/router_module';
-import { Router,CanActivate } from '@angular/router';
+
 
 
 @Component({
   selector: 'section',
   templateUrl: './sections.component.html',
   styleUrls: ['./sections.component.css'],
-  providers:[NewsService,ROUTER_PROVIDERS]
+  providers:[NewsService]
 })
 export class SectionsComponent implements OnInit {
 
   sectionList: section[]
-  constructor(private _newsService : NewsService,private router:Router) {
+  constructor(private _newsService : NewsService) {
   }
   ngOnInit() {
     this._newsService.getsections().subscribe((data) => this.sectionList = (data));
-    this.router.navigate(['Sections/News?section=world']);
   }
 
 }
